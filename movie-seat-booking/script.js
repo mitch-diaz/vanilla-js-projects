ae003bd8d910d03aa8abdc39aace36f03fb21594
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-05-09 22:56:02 -0400

    movie seat booking - added code to update the UI with local storage data

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 // `+` before converts movieSelect.value from string to number 
 // can also wrap variable like so -> parseInt(movieSelect.value)
 let ticketPrice = +movieSelect.value;
@@ -28,6 +30,25 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
@@ -44,7 +65,5 @@ container.addEventListener('click', (e) => {
   updateSelectedCount();
 });
 
-
-
-
-
+// Initial count and total set
+updateSelectedCount();

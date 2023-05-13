897d07302aecff43b045b47789cb00fe2aa6a43b
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-05-13 15:26:21 -0400

    dom-array-methods - added calculateWealth function using reduce method

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -34,6 +34,24 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money >= 1000000);
+
+  updateDOM()
+}
+
+// Calculate wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  console.log(wealth)
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+
+  // updateDOM();
+}
+
 // Sort users by wealth
 function sortByWealth() {
   data.sort((a, b) => b.money - a.money);
@@ -72,3 +90,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByWealth);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
